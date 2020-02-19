c50d9fc73d10f5359ba7b417e6115871fb096e5a
sinsys <nicholas.hazel@gmail.com>
2020-02-19 10:04:33 -0800

    Fixed port

--- server.js
+++ server.js
@@ -41,9 +41,7 @@ app.use((error, req, res, next) => {
   res.status(500).json(response)
 });
 
-const PORT = 
-  process.env.NODE_ENV ||
-  8000;
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
